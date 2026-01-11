c159b88b02fec6debe827bb84a85a92bd09c6cc8
AbdelrhmanMaged1 <abdelrhman.syam1@gmail.com>
2026-01-11 20:29:34 +0200

    Fix storage path for Vercel

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
@@ -10,8 +11,18 @@ require __DIR__ . '/../vendor/autoload.php';
 // 2. Bootstrap the Application
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// 3. FORCE STORAGE TO /tmp (Fixes Vercel Read-Only Error)
+// 3. FORCE STORAGE TO /tmp (Crucial for Vercel)
 $app->useStoragePath('/tmp/storage');
 
-// 4. Run the Application
-$app->handleRequest(Request::capture());
+// 4. Run the Application (Compatible with Laravel 9, 10, & 11)
+if (method_exists($app, 'handleRequest')) {
+    // Laravel 11 Logic
+    $app->handleRequest(Request::capture());
+} else {
+    // Laravel 9/10 Logic
+    $kernel = $app->make(Kernel::class);
+    $response = $kernel->handle(
+        $request = Request::capture()
+    )->send();
+    $kernel->terminate($request, $response);
+}
